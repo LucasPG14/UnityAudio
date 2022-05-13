78eb316e3776c19688e5f63712761a5270f12412
UriKurae <youtfrost@gmail.com>
2022-05-13 14:11:49 +0200

    3rd Person
    Co-Authored-By: Lucas Pérez <lucaspg14@gmail.com>

--- PracticaAudio/Asylum Audio/Assets/PlayerController.cs
+++ PracticaAudio/Asylum Audio/Assets/PlayerController.cs
@@ -7,30 +7,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     public float mouseSpeed = 5.0f;
     public SoundManager soundManager;
-    public Camera cam;
 
     float xRotation = 0.0f;
-
-    private void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(0.0f, 0.0f, Input.GetAxis("Vertical") * speed * Time.deltaTime);
-        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0.0f, 0.0f);
-
-        float mouseX = Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime;
-
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
-
-        cam.transform.localRotation = Quaternion.Euler(xRotation, 0.0f, 0.0f);
-        cam.transform.Rotate(Vector3.up * mouseX);
-    }
 
     public void OnTriggerEnter(Collider other)
     {
